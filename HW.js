4857dcbf44a627c6a2683d2252c1e70538b57981
astrouskaya76 <innadia2014@gmail.com>
2020-03-19 21:31:00 -0500

    AA

--- HW.js
+++ HW.js
@@ -1,6 +1,26 @@
+//
 let bool = true;
 let bool2 = false;
 let vv =  -0.78902;
 console.log(bool, bool2)
 console.log (bool,vv)
 console.log (vv, bool);
+//
+var x = 5.6;
+var y = 8.9;
+console.log ((x + y),(x - y),(x * y),(x / y),(x % y));
+
+//
+var d = -2;
+var d1 = d*=3; d1++
+console.log (d1);
+var d2 = d1-=2; d2/=3
+console.log (d2);
+var d3 = d2%=2; d3+=1
+console.log (d3);
+var d4 = d3-=1; d4*=4
+console.log (d3);
+var d5 = d4+=1; d5-=2
+console.log (d5);
+
+//
